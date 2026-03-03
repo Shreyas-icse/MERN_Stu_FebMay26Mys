9fbe7c39e73e32c50770e6ab4371d0782c30ce46
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-03 15:42:54 +0530

    Function declaration

---
+++ W3/D2/P5.js
@@ -0,0 +1,9 @@
+//Function declaration
+// function greet(name);
+//Function defination
+greet("shreyas");
+function greet(name) {
+    console.log("Hello, "+name+" !"); 
+}
+//function
+// greet("shreyas"); //(hoisting is supported)
