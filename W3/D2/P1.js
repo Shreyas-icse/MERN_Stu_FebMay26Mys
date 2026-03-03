f77d1255dc46e02d7815730180ee0ba2ff2b3295
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-03 15:41:02 +0530

    Arithmetic Operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,15 @@
+//Arithmetic Operators
+const a=2;
+const b = 2;
+//let c= 0;
+console.log("Arithmetic Operators");
+console.log("a+b: ",a+b);
+console.log("a-b: ",a-b);
+console.log("a*b: ",a*b);
+console.log("a/b: ",a/b);
+console.log("a%b: ",a%b); //modulus
+console.log("a**b: ",a**b);//exponential
+console.log("a++: ",a++); //post-increment
+console.log("++a: ",++a);// pre-increment
+console.log("a--: ",a--); //post-decrement
+console.log("--a: ",--a); //post-decrement
