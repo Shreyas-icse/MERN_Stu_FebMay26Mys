9ab5555543fc06b2f6564b4c6d5d8d761ec36042
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-03 15:41:52 +0530

    Comparison operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,13 @@
+//Comparison operators
+console.log("Comparison operators");
+let x=2;
+let y='3';
+console.log("x==y: ",x==y);//loose equality (only compares)
+console.log("x===y: ",x===y);//strict equality ( compares whith data type)
+console.log("x!=y: ",x!=y);//loose inequality (only compares)
+console.log("x!==y: ",x!==y);//strict inequality
+console.log("x>3: ",x>3);
+console.log("x<3: ",x<3);
+console.log("x>=3: ",x>=3);
+console.log("x<=3: ",x<=3);
+
