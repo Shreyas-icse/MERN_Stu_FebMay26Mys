33848c15d0b1a058a4fa6a270eb778b1a4ce29fc
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-03 15:42:16 +0530

    Logical operators

---
+++ W3/D2/P3.js
@@ -0,0 +1,6 @@
+//Logical operators
+console.log("Logical operators");
+let a = 1 ,b = 1;
+console.log("a&&b= ",a&&b);//logical AND
+console.log("a||b= ",a||b);//logical OR
+console.log("!a= ",!a);//logical NOT
