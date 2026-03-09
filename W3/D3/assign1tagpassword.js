a1e404e5b254d5e3156241c7b154025e58ded8b7
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-09 22:58:32 +0530

    assign tagpassword

---
+++ W3/D3/assign1tagpassword.js
@@ -0,0 +1,28 @@
+function tagpassword(password){
+   if (typeof password !== 'string') {
+        return "INVALID";
+    };
+const length= password.length;
+
+for(let i=0;i<=length;i++){
+    
+}
+
+if( length<8){
+    return("week");
+        
+}
+else if(length>=8 && length<12){
+   return("medium");
+   
+    
+}
+else if( length>=12){
+    return("strong");
+
+    
+}
+}
+console.log(tagpassword("shreyas.656565456"));
+
+
