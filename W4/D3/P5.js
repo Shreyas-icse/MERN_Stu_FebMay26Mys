67e27f248be153cf7aaf86ee8e7271af7617bf3c
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-11 16:35:03 +0530

    Introduction of Fetch API

---
+++ W4/D3/P5.js
@@ -0,0 +1,19 @@
+const introBtn = document.getElementById("introBtn");
+const output = document.getElementById("output");
+
+
+introBtn.addEventListener("click",function(){
+    output.textContent = "Sending request ....";
+    fetch("https://jsonplaceholder.typicode.com/posts/18")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log("Raw response object: ",data);
+        // output.textContent = "Status: "+response.status+"\nOk:"+response.ok;
+        output.textContent = JSON.stringify(data,null,2)
+    })
+    .catch(function(error){
+        output.textContent = "unexpected fetch error: "+error.message;
+    });
+});
