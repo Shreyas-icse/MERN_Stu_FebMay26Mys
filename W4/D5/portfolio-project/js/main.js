77f3881c7c6e8ab3b1dda56be979d028b67dea6f
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-14 12:29:09 +0530

    portfolio-main.js

---
+++ W4/D5/portfolio-project/js/main.js
@@ -0,0 +1,20 @@
+// document.addEventListener("DOMContentLoaded",function(){
+//     //Render skills
+//     renderSkills();
+// })
+// features/main(
+document.addEventListener("DOMContentLoaded",
+    function(){
+        //Render skills
+        renderSkills();
+        initModal();
+        initContactValidation();
+        initThemeToggle();
+    }
+);
+document.addEventListener("DOMContentLoaded", function(){
+
+    renderSkills();
+    renderProject();
+
+});
