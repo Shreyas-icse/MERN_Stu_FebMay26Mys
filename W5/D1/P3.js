18788c08f346b4569ce1d81d21ea90308a175a7d
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-16 16:23:53 +0530

    set timeout

---
+++ W5/D1/P3.js
@@ -0,0 +1,11 @@
+console.log("Node Js Architechure demo");
+console.log("1.script started");
+
+//set timeout
+setTimeout(()=>{
+    console.log("3. timer callback finished after waiting");
+    
+},10000);
+console.log("2. Script continued without waiting for timer callback");
+
+
