a19ac56eabb6d7635258bcf876c438254c19e331
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-17 13:21:26 +0530

    callback errors

---
+++ W5/D2/P5.js
@@ -0,0 +1,18 @@
+// callback errors
+
+function divideNumbers(a,b,callback){
+    if(b==0){
+        callback("Cannot divide by zero.",null);
+        return;
+    }
+    const result = a/b;
+    callback(null,result);
+}
+divideNumbers(10,0,function(error,result){
+    if(error){
+        console.log("Error: ",error);
+        return;
+    }
+    console.log("Result: ",result);
+    
+});
