aee824572b68dd66c1aff7be51c4a533074576fc
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-17 13:20:46 +0530

    Asynchronous approach of Noded.js

---
+++ W5/D2/P3.js
@@ -0,0 +1,19 @@
+// Asynchronous approach of Noded.js
+
+console.log("Step 1:Script started.");
+
+setTimeout(()=>{
+
+},1000);
+
+setTimeout(function(){
+    console.log("Step 3: Delayed callback finished.F1.");
+    
+},8000);
+setTimeout(function(){
+    console.log("Step 3: Delayed callback finished.F2.");
+    
+},3000);
+
+console.log("step 2: Script did not stop while waiting");
+
