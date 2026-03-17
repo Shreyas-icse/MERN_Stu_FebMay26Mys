e7f55da6a91cdb4591c3e187df20dfc43991ca9f
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-17 13:21:04 +0530

    Writing custom callback function

---
+++ W5/D2/P4.js
@@ -0,0 +1,42 @@
+// Writing custom callback function
+function processstudent(name,score,callback,monkey){
+    console.log("Student name : ",name);
+    console.log("Score: ",score);
+    callback(name,score);
+    monkey(name,score);
+    
+    
+}
+function showGrade(name,score){
+    if(score>=85){
+        console.log("Grade: A+");
+        
+    }
+    else if(score>=75){
+        console.log("Grade: A");
+        
+    }
+    else if(score>=70){
+        console.log("Grade: B");
+        
+    }
+    else{
+        console.log("Grade: fail");
+        
+    }
+}
+function showResult(name,score){
+    if(score>=70){
+        console.log(name+ " has passed.");
+        
+    }
+    else{
+        console.log(name+ " has failed.");
+        
+    }
+}
+
+
+
+processstudent("shreyas",96,showResult,showGrade);
+// processstudent("shreyas",96,showGrade);
