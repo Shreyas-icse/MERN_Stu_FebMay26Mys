c4de3bceb44d08df85f0a49318a12d50a42077f8
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-23 12:32:13 +0530

    Basics Node.Js Modules

---
+++ W6/D1/P1.js
@@ -0,0 +1,8 @@
+//Basics Node.Js Modules
+const moduleTitle = "NodeJs module basics";
+function describeModule(){
+    console.log("This file is its own module");
+    console.log("Title: ",moduleTitle);
+    console.log("Local values stay inside this file unless exported");
+}
+describeModule(); //invoking the function
