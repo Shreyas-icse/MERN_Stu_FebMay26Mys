3ddd89254e26ebd7c5f2c8af0eeff5774c3d34da
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-26 12:32:57 +0530

    Reading the POST request body data

---
+++ W6/D4/P5.js
@@ -0,0 +1,26 @@
+//Reading the POST request body data
+const http = require("http");
+
+const server = http.createServer(function (req, res) {
+    //-X POST http://localhost:3001/submit -d "name=shreyas&role=admin"
+    if (req.url === "/submit" && req.method === "POST") {
+        let body = "";
+        //req here is a readable stream
+        req.on("data", function (chunk) {
+            body += chunk.toString();
+        });
+        //'end' executes when full request body has been recived
+        req.on("end", function () {
+            res.writeHead(200, { "content-Type": "text/plain" });
+            res.end("Recived req body data: " + body);
+        });
+        return;
+
+    }
+    res.writeHead(404,{"Content-Type":"text/plain"});
+    res.end("Route not found.");
+});
+
+server.listen(3001,function(){
+    console.log("server is running at http://localhost:3001");   
+});
