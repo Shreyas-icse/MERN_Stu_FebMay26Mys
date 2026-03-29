5a8a1f232ceda791ff7770205996a8ee464e5000
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-30 00:16:08 +0530

    data of skills

---
+++ Portfolio-project-v3/js/data/skill.js
@@ -0,0 +1,8 @@
+const skills = [
+    { name: "HTML", category: "frontend" },
+    { name: "CSS", category: "frontend" },
+    { name: "JavaScript", category: "frontend" },
+    { name: "Node.js", category: "backend" },
+    { name: "Express", category: "backend" },
+    { name: "Git", category: "tools" }
+];
