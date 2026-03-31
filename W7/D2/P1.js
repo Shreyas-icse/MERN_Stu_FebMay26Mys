6ead833fe7662f088dc73d1104d3e02bd6c89a1c
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-03-31 17:11:07 +0530

    Routing : Nested routes

--- W7/D2/P1.js
+++ W7/D2/P1.js
@@ -1,32 +1,55 @@
-//Routing : Nested routes
-// "/" base url
+// Routing: Nested routes
+//  "/" base url
 // "/api/users"
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
-//"/api/users" /create/delete/update/:id
-
-//router objects help organize route groups.
-
+// Router objects help organize route Groups
 const apiRouter = express.Router();
 
 apiRouter.get("/users",function(req,res){
     res.json({
-        route:"/api/users",
-        message:"Users route inside api router"
+        route: "/api/users",
+        message: "Users route inside api router"
     });
 });
+
 apiRouter.get("/orders",function(req,res){
     res.json({
-        route:"/api/orders",
-        message:"Orders route inside api router"
+        route: "/api/orders",
+        message: "Orders route inside api router"
     });
 });
-//Mounting the router under /api base path.
+
+// Mount the router under the /api base path
 app.use("/api",apiRouter);
+
+// ***New Router***
+// Products router (handles/api/products/...)
+const productsRouter = express.Router();
+
+// /api/products    ->create product
+productsRouter.post("/", (req,res)=>{
+    res.json({
+        route: "/api/products",
+        message: "Create products"
+    });
+});
+
+// /api/products/:id    ->delete product
+productsRouter.delete("/:id",(req,res)=>{
+    res.json({
+        route: `/api/products/${req.params.id}`,
+        message: "Delete product"
+    });
+});
+
+// mount products router under /api/products
+apiRouter.use("/products",productsRouter)
+
 
 app.listen(4000,function(){
     console.log("Express server running at http://localhost:4000");
-    
 });
+//curl -X DELETE http://localhost:4000/api/products/123
+//curl -X POST http://localhost:4000/api/products -H "Content-Type: application/json" -d "{"name":"pen","price":10}"
