dd52e6cd7fbbb982e9a16b44f4866bb7432dbced
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-04-21 15:01:00 +0530

    added auth routes

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const authRoutes = require("./routes/auth.routes")
 const app = express();
 
 app.use(express.json());
-
-app.get("/",(res,req)=>{
+app.use("/api/auth",authRoutes);
+//Base URL
+app.get("/",(req,res)=>{
     res.status(200).json({
          success:true,
     message:"movie booking API is running.."
