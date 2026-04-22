f973e8b5e8a47a00e0a64076db2c309cbdfc98fc
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-04-22 09:49:25 +0530

    created Auth middleware in which verified token and fetched user details

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/auth.middleware.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+const user = require("../models/User");
+
+//Auth middleware
+exports.protect = async (req, res, next) => {
+    try {
+        let token;
+        if (
+            req.headers.authorization && req.headers.authorization.startsWith("Bearer")
+        ) {
+            token = req.authorization.split(" ")[1];
+        }
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Not authorized, token missing",
+            });
+        }
+        //Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        //Get user from db
+        const user = await User.findById(decoded.id);
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+        //attach user to request
+        req.user = user;
+        next();
+    }
+    catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "invalid or expired token",
+        });
+    }
+}
