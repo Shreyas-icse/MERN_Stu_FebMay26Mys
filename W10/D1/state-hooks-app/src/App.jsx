83132cbd5fe3f14bd45c200e9931bf0117e4add4
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-05-04 10:47:39 +0530

    state-hooks-app basics

---
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {UseStateBasics} from './components/P1'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <UseStateBasics/>
+    </>
+  )
+}
+
+export default App
