d1fa25e499eefa2081fc6983e836a664b7b26c19
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-05-06 14:08:53 +0530

    useCallback hook

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import { UseContextIntro } from './components/P1'
 import { CustomHooksIntro } from './components/P2'
+import { UserRefIntro } from './components/P3'
+import { UseReducerIntro } from './components/P4'
+import { UseCallbackIntro } from './components/P5'
 import './App.css'
 
 function App() {
@@ -8,8 +11,11 @@ function App() {
 
   return (
     <>
-     <UseContextIntro/>
+     {/* <UseContextIntro/> */}
      {/* <CustomHooksIntro/> */}
+     {/* <UserRefIntro/> */}
+     {/* <UseReducerIntro/> */}
+     <UseCallbackIntro/>
     </>
   )
 }
