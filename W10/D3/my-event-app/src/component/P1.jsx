4f1fabdbcde62fe01eec7212b1b4a287e61466d0
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-05-07 09:56:58 +0530

    Basic events in React

--- W10/D3/my-event-app/src/component/P1.jsx
+++ W10/D3/my-event-app/src/component/P1.jsx
@@ -1,1 +1,20 @@
+//Basic events in React
+//What is an event?
+//An action triggerd by the user (mouse, keyboard, DOM)
+//React uses camelCase attributes like onClick, onChange...
+//React passes an event object (syntheticEvent) to the handler
 
+export function EventBasics(){
+    //Declaring a event handler function
+    const handleClick = () => alert("Clicked");
+
+    return(
+        <section>
+            <h2>Basic Events</h2>
+            {/* Event binding */}
+            <button onClick={handleClick}>
+                Clicke me
+            </button>
+        </section>
+    )
+}
