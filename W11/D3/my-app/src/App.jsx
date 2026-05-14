8ed73642b54d3436ff8299ac618e17bc5d206270
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-05-14 10:39:01 +0530

    useEffect

---
+++ W11/D3/my-app/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+import { ManagingApiState } from "./components/p1";
+import { UseEffectLifeCycle } from "./components/p2";
+import { AxiosLifeCycle } from "./components/p3";
+import { CRUDoperation } from "./components/p4";
+import { PaginationCaching } from './components/P5';
+
+function App() {
+  return (
+    <>
+      {/* <ManagingApiState /> */}
+
+      {/* <UseEffectLifeCycle /> */}
+      {/* <AxiosLifeCycle/> */}
+      {/* <CRUDoperation/> */}
+      <PaginationCaching/>
+
+    </>
+  );
+}
+
+export default App;
