7ee42daffc2d9a8bc4fd335c6b294c8c0b4a6042
Shreyas-icse <shreyasshreyas925@gmail.com>
2026-06-12 12:04:41 +0530

    usage of props

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,15 +1,34 @@
 
 import './App.css'
 
-import HomePage from './pages/HomePage'
+import MovieCard from './components/MovieCard'
 function App() {
  
   return (
       //Component Composition
-    <HomePage/>
-       
+      <>
+    <MovieCard
+    title="Inception"
+    genre="Scifi"
+    rating={4.8}
+    duration="2h 28m"
+    poster="https://picsum.photos/300/300?1"/>
 
-  )
+     <MovieCard
+    title="Toxic"
+    genre="Action"
+    rating={9.8}
+    duration="2h 28m"
+    poster="https://picsum.photos/300/300?2"/>
+    <MovieCard
+    title="RCB"
+    genre="Action"
+    rating={9.8}
+    duration="2h 28m"
+    poster="https://picsum.photos/300/300?3"/>
+      </> 
+
+  );
 }
 
 export default App
